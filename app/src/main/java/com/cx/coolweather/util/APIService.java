963edd271a7b78d0832b7b2b4a1aa46bfdfc4c9e
chu <chuxiongc@gmail.com>
2017-07-27 18:38:21 +0800

    添加retrofit和rxjava

---
+++ app/src/main/java/com/cx/coolweather/util/APIService.java
@@ -0,0 +1,12 @@
+package com.cx.coolweather.util;
+
+/*
+ *  @项目名：  CoolWeather 
+ *  @包名：    com.cx.coolweather.util
+ *  @文件名:   APIService
+ *  @创建者:   YHDN
+ *  @创建时间:  2017-07-27 18:34
+ *  @描述：    TODO
+ */
+
+public interface APIService {}
